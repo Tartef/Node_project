bd9c4f79ef857bf1c2398abc84920529ae934827
Devin Ivy <devin@bigroomstudios.com>
2021-04-11 23:24:17 -0400

    Initial commit

--- server/manifest.js
+++ server/manifest.js
@@ -13,14 +13,14 @@ module.exports = new Confidence.Store({
     server: {
         host: 'localhost',
         port: {
-            $param: 'PORT',
+            $env: 'PORT',
             $coerce: 'number',
             $default: 3000
         },
         debug: {
-            $filter: 'NODE_ENV',
+            $filter: { $env: 'NODE_ENV' },
             $default: {
-                log: ['error', 'start'],
+                log: ['error'],
                 request: ['error']
             },
             production: {
@@ -35,35 +35,34 @@ module.exports = new Confidence.Store({
                 options: {}
             },
             {
-<<<<<<< HEAD
-                plugin: '@hapipal/schwifty',
-                options: {
-                    $filter: 'NODE_ENV',
-                    $default: {},
-                    $base: {
-                        migrateOnStart: true,
-                        knex: {
-                            client: 'sqlite3',
-                            useNullAsDefault: true,     // Suggested for sqlite3
-                            connection: {
-                                filename: ':memory:'
-                            },
-                            migrations: {
-                                stub: Schwifty.migrationsStubPath
+                plugin: './plugins/swagger'
+            },
+            {
+                plugin  : '@hapipal/schwifty',
+                options : {
+                    $filter    : 'NODE_ENV',
+                    $default   : {},
+                    $base      : {
+                        migrateOnStart : true,
+                        knex           : {
+                            client     : 'mysql2',
+                            connection : {
+                                host     : process.env.DB_HOST,
+                                user     : process.env.DB_USER,
+                                password : process.env.DB_PASSWORD,
+                                database : process.env.DB_DATABASE,
+                                port: 3307
                             }
                         }
                     },
-                    production: {
-                        migrateOnStart: false
+                    production : {
+                        migrateOnStart : false
                     }
                 }
-=======
-                plugin: './plugins/swagger'
->>>>>>> 2a479ff ((flavor) swagger v3.0.0)
             },
             {
                 plugin: {
-                    $filter: 'NODE_ENV',
+                    $filter: { $env: 'NODE_ENV' },
                     $default: '@hapipal/hpal-debug',
                     production: Toys.noop
                 }
